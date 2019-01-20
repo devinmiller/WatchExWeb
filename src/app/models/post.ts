119640957e69dcdc16547771982c833c484d418b
Devin Miller <devinmiller@codeonthebrain.com>
2019-01-19 17:03:27 -0700

    Initial check in of project.

---
+++ src/app/models/post.ts
@@ -0,0 +1,13 @@
+export class Post {
+    Id:string;
+    Author:string;
+    IsMeta:boolean;
+    IsSelf:boolean;
+    LinkFlairText:string;
+    Permalink:string;
+    Pinned:boolean;
+    Stickied:boolean;
+    Timestamp:Date;
+    Title:string;
+    Url:string;
+}
