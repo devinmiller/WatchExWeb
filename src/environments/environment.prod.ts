9c60da217eeeebf3783adfad73cb0989bcb3b6d6
Devin Miller <devinmiller@codeonthebrain.com>
2019-04-28 13:35:01 -0700

    Added Identity settings to prod config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  searchUrl: 'https://wex-api.codeonthebrain.com/api/post'
+  searchUrl: 'https://wex-api.codeonthebrain.com/api/post',
+  authentication: {
+    authority: 'https://identity.codeonthebrain.com/',
+    redirectUri: 'https://wex.cotb.dev/auth/callback',
+    logoutUri: 'https://wex.cotb.dev/'
+  }
 };
